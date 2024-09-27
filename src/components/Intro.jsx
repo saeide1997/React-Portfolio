69318a008cfdba5e6a7f7d13ae1acf07fc9499cd
saeide <saeide.tj@gmail.com>
2024-09-27 14:27:37 +0330

    intro edition

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -18,19 +18,23 @@ const Intro = () => {
     return (
         
         <div className='bg-white  flex max-[1000px]:flex-col max-[1000px]:items-center'  id="Intro">
-            <div className="flex1 relative w-[100%]">
+            <div className="flex-1 relative w-[100%]">
                 <div className="w-[100%] h-[100%] mr-10 pl-12 flex flex-col justify-center max-[1000px]:pl-0 max-[1000px]:items-center max-[1000px]:mr-0">
-                    <h2 className="text-[45px] my-3">سلاملکوم</h2>
-                    <h1 className="text-[60px] max-[1000px]:text-[30px] my-3">بنده سویده هستم </h1>
-                    <h3 className="text-[30px] my-3 max-[1000px]:text-[20px]">برنامه نویس <span className='text-violet-600 text-[45px] max-[1000px]:text-[20px]' ref={textRef}></span></h3>
+                    <h2 className="text-[40px] my-3">سلام</h2>
+                    <h1 className="text-[50px] max-[1000px]:text-[30px] my-3">من سعیده تاجمهر هستم </h1>
+                    <h3 className="text-[35px] my-3 max-[1000px]:text-[20px]">برنامه نویس <span className='text-violet-600 text-[45px] max-[1000px]:text-[20px]' ref={textRef}></span></h3>
+                    <div className='flex '>
+                        <a className="text-[20px] text-white flex justify-center items-center p-5 ml-20 mt-20 border-solid border-violet-800 border-2 bg-violet-500 rounded-lg bottom-14 right-[20%]" href="#Portfolio">درباره من</a>
+                        <a className="text-[20px] text-white flex justify-center items-center p-5 mt-20 border-solid border-violet-800 border-2 bg-violet-500 rounded-lg bottom-14 right-[20%]" href="#Portfolio">تماس من</a>
+                    </div>
                 </div>
                 <a className="absolute bottom-14 right-[20%]" href="#Portfolio">
                     <img className="w-[30px]" style={{animation:"arrowBlink 2s infinite"}} src="assets/down.png" alt="" />
                 </a>
             </div>
-            <div className="flex2 overflow-hidden">
+            <div className="max-[640px]:flex-1 flex-2 overflow-hidden">
                 <div className="w-[900px] mt-[40px] h-[900px] bg-violet-500/50 rounded-full flex items-end justify-center float-left max-[1000px]:items-start">
-                    <img className=" object-contain h-full w-full " src="assets/sav1.png" alt="" />
+                    <img className="max-[640px]:w-[60%] max-[640px]:h-[55%] object-contain h-full w-full " src="assets/sav1.png" alt="" />
                 </div>
             </div>
             
